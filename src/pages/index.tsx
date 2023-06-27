b574e29af2c26f74f791cf65bcfa18e9f936a494
Ged <Ged.stenman@gmail.com>
2023-06-27 14:34:18 +0300

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  const user = useUser();
+  
+
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -40,10 +45,19 @@ export default function Home() {
                 to deploy it.
               </div>
             </Link>
-          </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+          </div>  
+        </div>
+        <div>
+          {!user.isSignedIn && <SignInButton /> }
+          {!!user.isSignedIn && <SignOutButton /> }
+        </div>
+        
+        {!!user.isSignedIn && <div>{user.user.id}</div>}
+        
+        <div>
+           
+           {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+          
         </div>
       </main>
     </>
